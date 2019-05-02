938ced59d21a7dc7b4b0af59dfc1fc4862461dc2
Guillaume Charmetant <cguille.dev@gmail.com>
2019-05-02 14:49:10 +0200

    implement day 1 part 2

---
+++ src/bin/part2.rs
@@ -0,0 +1,29 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::collections::HashSet;
+use std::process;
+
+fn main() {
+    let mut seen_frequencies = HashSet::new();
+    let mut frequency = 0;
+
+    seen_frequencies.insert(frequency);
+
+    loop {
+        let file = File::open("input.txt").expect("Input file could not be opened");
+
+        for line in BufReader::new(file).lines() {
+            let line = line.expect("Line could not be read from buffer");
+            let change = line.parse::<i32>().expect("Could not parse line into i32");
+
+            frequency += change;
+
+            let already_seen = !seen_frequencies.insert(frequency);
+
+            if already_seen {
+                println!("{}", frequency);
+                process::exit(0);
+            }
+        }
+    }
+}
