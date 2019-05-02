5cf24d3eaefc3e527fe6d4f255a8ab4dad366eb0
Guillaume Charmetant <cguille.dev@gmail.com>
2019-05-02 14:32:32 +0200

    implement part one

---
+++ src/bin/part1.rs
@@ -0,0 +1,16 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let file = File::open("input.txt").expect("Input file could not be opened");
+    let mut frequency = 0;
+
+    for line in BufReader::new(file).lines() {
+        let line = line.expect("Line could not be read from buffer");
+        let change = line.parse::<i32>().expect("Could not parse line into i32");
+
+        frequency += change;
+    }
+
+    println!("{}", frequency);
+}
