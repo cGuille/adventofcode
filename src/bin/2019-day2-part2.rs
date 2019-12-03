24fd63a85fb20aa0eebd37f69955e77feb944d15
Guillaume Charmetant <cguille.dev@gmail.com>
2019-12-03 08:56:25 +0100

    Add src/bin/2019-day2-part2.rs

---
+++ src/bin/2019-day2-part2.rs
@@ -0,0 +1,24 @@
+use adventofcode::intcode::Computer;
+use std::fs;
+use std::process;
+
+fn main() {
+    let source_code = fs::read_to_string("input/2019-day2.txt");
+    let source_code = source_code.expect("Input file could not be opened");
+    let mut computer = Computer::init(&source_code);
+
+    let searched_result = 19690720;
+
+    for noun in 0..100 {
+        for verb in 0..100 {
+            let result = computer.run(noun, verb);
+
+            if result == searched_result {
+                println!("{}", 100 * noun + verb);
+                process::exit(0);
+            }
+        }
+    }
+
+    process::exit(1);
+}
