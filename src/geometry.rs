ec6777ffcc66b05c0b299cff036402e9f5e2796c
Guillaume Charmetant <cguille.dev@gmail.com>
2019-12-04 13:06:56 +0100

    Add crate::geometry::Point

---
+++ src/geometry.rs
@@ -0,0 +1,46 @@
+extern crate num_traits;
+
+use num_traits::Signed;
+use std::ops::Sub;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T: Sub + Signed + Copy> Point<T> {
+    pub fn new(x: T, y: T) -> Point<T> {
+        Point { x: x, y: y }
+    }
+
+    pub fn x(&self) -> T {
+        self.x
+    }
+
+    pub fn y(&self) -> T {
+        self.y
+    }
+
+    pub fn manhattan_distance(&self, other: &Point<T>) -> T {
+        (self.x - other.x).abs() + (self.y - other.y).abs()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::geometry::Point;
+
+    #[test]
+    fn test_manhattan_distance() {
+        let p1 = Point::new(0, 0);
+        let p2 = Point::new(0, 0);
+
+        assert_eq!(p1.manhattan_distance(&p2), 0);
+        assert_eq!(p2.manhattan_distance(&p1), 0);
+
+        let p2 = Point::new(1, 1);
+        assert_eq!(p1.manhattan_distance(&p2), 2);
+        assert_eq!(p2.manhattan_distance(&p1), 2);
+    }
+}
