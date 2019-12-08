345de87527eb8ba8b5dbeede4cc0c4dcc46c2aab
Guillaume Charmetant <cguille.dev@gmail.com>
2019-12-08 22:17:35 +0100

    Implement parameter modes

--- src/intcode.rs
+++ src/intcode.rs
@@ -7,6 +7,75 @@ pub struct Computer {
     pointer: usize,
 }
 
+#[derive(Debug)]
+struct Instruction {
+    opcode: Opcode,
+    param_modes: Vec<ParameterMode>,
+}
+
+impl From<i32> for Instruction {
+    fn from(mut number: i32) -> Self {
+        if number < 0 {
+            panic!("Invalid instruction: {}", number);
+        }
+
+        let mut digits = Vec::new();
+
+        while number > 9 {
+            digits.push(number % 10);
+            number = number / 10;
+        }
+        digits.push(number);
+
+        let opcode_digits = [digits.get(1).unwrap_or(&0), digits.get(0).unwrap_or(&0)];
+        let opcode = Opcode::from(opcode_digits);
+        let param_modes = if digits.len() < 3 {
+            Vec::new()
+        } else {
+            digits[2..].iter().map(ParameterMode::from).collect()
+        };
+
+        Instruction {
+            opcode: opcode,
+            param_modes: param_modes,
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Opcode {
+    Add,
+    Multiply,
+    Exit,
+}
+
+impl From<[&i32;2]> for Opcode {
+    fn from(digits: [&i32;2]) -> Self {
+        match digits {
+            [0, 1] => Opcode::Add,
+            [0, 2] => Opcode::Multiply,
+            [9, 9] => Opcode::Exit,
+            _ => panic!("Unknown opcode {:?}", digits),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum ParameterMode {
+    Pointer,
+    Value,
+}
+
+impl From<&i32> for ParameterMode {
+    fn from(digit: &i32) -> Self {
+        match digit {
+            0 => ParameterMode::Pointer,
+            1 => ParameterMode::Value,
+            _ => panic!("Not a parameter mode {}", digit),
+        }
+    }
+}
+
 impl Computer {
     pub fn init(source_code: &str) -> Computer {
         let opcodes: Vec<i32> = source_code.trim().split(",").map(str_to_i32).collect();
@@ -37,14 +106,12 @@ impl Computer {
                 panic!("Unexpected end of program: {:?}", self);
             }
 
-            match self.memory[self.pointer] {
-                1 => self.add(),
-                2 => self.multiply(),
-                99 => break, // End of program
-                _ => panic!(
-                    "{} is not an instruction in {:?}",
-                    self.memory[self.pointer], self
-                ),
+            let instruction = Instruction::from(self.memconsume());
+
+            match instruction.opcode {
+                Opcode::Add => self.add(instruction.param_modes),
+                Opcode::Multiply => self.multiply(instruction.param_modes),
+                Opcode::Exit => break,
             };
         }
     }
@@ -57,26 +124,34 @@ impl Computer {
         value
     }
 
-    fn add(&mut self) {
-        let _instruction = self.memconsume();
+    fn memconsumearg(&mut self, mode: &ParameterMode) -> i32 {
+        let arg = self.memconsume();
 
-        let left_op_pos: usize = self.memconsume().try_into().expect("Invalid position");
-        let right_op_pos: usize = self.memconsume().try_into().expect("Invalid position");
-        let result_pos: usize = self.memconsume().try_into().expect("Invalid position");
-
-        let result = self.memory[left_op_pos] + self.memory[right_op_pos];
-        self.memory[result_pos] = result;
+        match mode {
+            ParameterMode::Value => arg,
+            ParameterMode::Pointer => {
+                let pointer: usize = arg.try_into().expect("Invalid pointer");
+                self.memory[pointer]
+            }
+        }
     }
 
-    fn multiply(&mut self) {
-        let _instruction = self.memconsume();
+    fn add(&mut self, param_modes: Vec<ParameterMode>) {
+        let left_op = self.memconsumearg(param_modes.get(0).unwrap_or(&ParameterMode::Pointer));
+        let right_op = self.memconsumearg(param_modes.get(1).unwrap_or(&ParameterMode::Pointer));
 
-        let left_op_pos: usize = self.memconsume().try_into().expect("Invalid position");
-        let right_op_pos: usize = self.memconsume().try_into().expect("Invalid position");
-        let result_pos: usize = self.memconsume().try_into().expect("Invalid position");
+        let result_pointer: usize = self.memconsume().try_into().expect("Invalid pointer");
 
-        let result = self.memory[left_op_pos] * self.memory[right_op_pos];
-        self.memory[result_pos] = result;
+        self.memory[result_pointer] = left_op + right_op;
+    }
+
+    fn multiply(&mut self, param_modes: Vec<ParameterMode>) {
+        let left_op = self.memconsumearg(param_modes.get(0).unwrap_or(&ParameterMode::Pointer));
+        let right_op = self.memconsumearg(param_modes.get(1).unwrap_or(&ParameterMode::Pointer));
+
+        let result_pointer: usize = self.memconsume().try_into().expect("Invalid pointer");
+
+        self.memory[result_pointer] = left_op * right_op;
     }
 }
 
